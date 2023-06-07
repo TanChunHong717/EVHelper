4948e1c0f3b967430ce6f361f23e6f4853da95d7
TanChunHong <tanchunhong717@gmail.com>
2023-06-08 00:03:00 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.sidebar.markdown("# Electric Car Helper")
 st.markdown("EVHelper is a web app that assists users in selecting the right electric vehicle (EV). It offers two features: analyzing accidents involving Level 2 ADAS and providing detailed EV specifications. By offering accident data and comprehensive specifications, EVHelper helps users make informed EV choices.")
 
 st.markdown('''
-    <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px;">
+    <div style="background-color: #FF4B4B; padding: 10px; border-radius: 5px;">
         <h4>Tip 1: Assess your needs</h4>
         <p>Evaluate your daily driving habits, including mileage and commuting distance, to determine the range requirements for your EV. Choose a model with a range that comfortably meets your needs.</p>
     </div><br>
@@ -13,7 +13,7 @@ st.markdown('''
 
 # Tip 2: Compare specifications
 st.markdown('''
-    <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px;">
+    <div style="background-color: #FF4B4B; padding: 10px; border-radius: 5px;">
         <h4>Tip 2: Compare specifications</h4>
         <p>Research and compare EV models from different manufacturers. Consider factors such as battery capacity, charging time, available features, and pricing to find the best match for your preferences and budget.</p>
     </div><br>
@@ -21,7 +21,7 @@ st.markdown('''
 
 # Tip 3: Check incentives and charging infrastructure
 st.markdown('''
-    <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px;">
+    <div style="background-color: #FF4B4B; padding: 10px; border-radius: 5px;">
         <h4>Tip 3: Check incentives and charging infrastructure</h4>
         <p>Look into government incentives, tax credits, and rebates that can reduce the cost of owning an EV. Additionally, assess the availability of charging stations in your area, both at home and on the road, to ensure convenient charging options.</p>
     </div><br>
